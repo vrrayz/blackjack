3dcb232444509151890bdae135f53c8bf6ebb047
Victor <emmyvic98@gmail.com>
2023-01-23 13:27:23 +0000

    Don't calculate hidden cards

--- src/logic/rankScore.js
+++ src/logic/rankScore.js
@@ -1,19 +1,22 @@
 export const rankScore = (cards) => {
   const tempArray = [];
   cards.forEach((card, index) => {
-    // Differentitate the string ranks like A J Q K with the number ranks
-    if (typeof card.rank === "string") {
-      // Logic for when A should be 10 and when A should be 1
-      if (card.rank === "A") {
-        const ts = totalScore(tempArray, index);
-        const score = ts + 10 > 21 ? 1 : 10;
-        tempArray.push({ rank: card.rank, score: score });
+    // Don't add the hidden cards
+    if (!card.isHidden) {
+      // Differentitate the string ranks like A J Q K with the number ranks
+      if (typeof card.rank === "string") {
+        // Logic for when A should be 10 and when A should be 1
+        if (card.rank === "A") {
+          const ts = totalScore(tempArray, index);
+          const score = ts + 10 > 21 ? 1 : 10;
+          tempArray.push({ rank: card.rank, score: score });
+          return tempArray;
+        }
+        tempArray.push({ rank: card.rank, score: 10 });
         return tempArray;
       }
-      tempArray.push({ rank: card.rank, score: 10 });
-      return tempArray;
+      tempArray.push({ rank: card.rank, score: card.rank });
     }
-    tempArray.push({ rank: card.rank, score: card.rank });
     return tempArray;
   });
   return totalScore(tempArray, -1);
